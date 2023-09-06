e8b21b9658c1b011bd0bc5af57e903d66dda545b
laurilauter <lauri.lauter@gmail.com>
2023-09-06 23:59:14 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EX/ex01_hello/hello.py

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,11 +1,9 @@
-"""
-Write a program that prints 'Hello World!' to the screen.
-"""
+"""Write a program that prints 'Hello World!' to the screen."""
 
 
 def main():
     """Print 'Hello World!'."""
-    print("Hello, world!")
+    print("Hello world!")
 
 
 if __name__ == '__main__':
