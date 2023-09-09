83e703e8f9f3cc6492e69cfc365f53448f0e55bd
laurilauter <lauri.lauter@gmail.com>
2023-09-09 23:19:43 +0300

    Adding more files.

--- EX/ex02_beginning/maths.py
+++ EX/ex02_beginning/maths.py
@@ -34,7 +34,7 @@ def main():
     Output
     6
 
-     """
+    """
     a = int(input("Enter the value of a: "))
     b = int(input("Enter the value of b: "))
     c = int(input("Enter the value of c: "))
