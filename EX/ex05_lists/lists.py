5c632a7ee7cb3498cf6dc2aa0dad681ced93fc68
laurilauter <lauri.lauter@gmail.com>
2023-10-05 23:09:49 +0300

    Added lists.py.

---
+++ EX/ex05_lists/lists.py
@@ -0,0 +1,96 @@
+"""Phone inventory."""
+
+
+def list_of_phones(all_phones: str) -> list:
+    """
+    Return list of phones.
+
+    The input string contains of phone brands and models, separated by comma.
+    Both the brand and the model do not contain spaces (both are one word).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google Pixel', 'Honor Magic5', 'Google Pixel"]
+    """
+    return all_phones.split(", ")
+
+
+def phone_brands(all_phones: str) -> list:
+    """
+    Return list of unique phone brands.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google", "Honor"]
+    """
+    phones = all_phones.split(",")
+    brands = []
+    for phone in phones:
+        brand = phone.split(" ")[0]
+        if brand not in brands:
+            brands.append(brand)
+    return brands
+
+
+def phone_models(all_phones: str) -> list:
+    """
+    Return list of unique phone models.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Honor Magic5,Google Pixel,Honor Magic4" => ['Magic5', 'Pixel', 'Magic4']
+    """
+    phones = all_phones.split(",")
+    models = []
+    for phone in phones:
+        phone_fragments = phone.split(" ")
+        model = phone_fragments[1:]
+        if model not in models:
+            models.append(" ".join(model))
+    return models
+
+
+def search_by_brand(all_phones: str, search_term: str) -> list:
+    """Return phone by brand."""
+    phones = all_phones.split(",")
+    search_results = []
+    for phone in phones:
+        phone_fragments = phone.split(" ")
+        phone_fragments = phone_fragments[:1]
+        for i in phone_fragments:
+            if search_term.lower() in i.lower() and len(search_term) == len(i):
+                search_results.append(phone)
+    return search_results
+
+
+def search_by_model(all_phones: str, search_term: str) -> list:
+    """Return phone by model."""
+    phones = all_phones.split(",")
+    search_results = []
+    for phone in phones:
+        phone_fragments = phone.split(" ")
+        phone_fragments = phone_fragments[1:]
+        for i in phone_fragments:
+            if search_term.lower() in i.lower() and len(search_term) == len(i):
+                search_results.append(phone)
+    return search_results
+
+
+if __name__ == '__main__':
+
+    print(list_of_phones("Google Pixel,Honor Magic5,Google Pixel"))  # ["Google Pixel', 'Honor Magic5', 'Google Pixel"]
+    print(phone_brands(
+        "Google Pixel,Honor Magic5,Google Pix,Honor Magic6,IPhone 12,Samsung S10,Honor Magic,IPhone 11"))  # ['Google', 'Honor', 'IPhone', 'Samsung']
+    print(phone_brands("Google Pixel,Google Pixel,Google Pixel,Google Pixel"))  # ['Google']
+    print(phone_brands(""))  # []
+    print(phone_models("IPhone 14,Google Pixel,Honor Magic5,IPhone 14"))  # ['14', 'Pixel', 'Magic5']
+    print(phone_models("Samsung Galaxy S23,IPhone 14 Pro Max"))  # ['14', 'Pixel', 'Magic5']
+
+    print(search_by_brand("IPhone 14,iphone 7,IPHONE 11 Pro, Something", "IPhone"))  # ['14', 'Pixel', 'Magic5']
+    print(search_by_brand("IPhone 14,iphone 7,IPHONE 11 Pro, Something", "Pro"))  # ['14', 'Pixel', 'Magic5']
+
+    print(search_by_model("IPhone X,IPhone 12 Pro,IPhone 14 pro Max,Something", "Pro"))  # ['14', 'Pixel', 'Magic5']
+    print(search_by_model("IPhone X,IPhone 12 Pro,IPhone 14 pro Max", "14"))  # ['14', 'Pixel', 'Magic5']
+    print(search_by_model("IPhone X,IPhone 12 Pro,IPhone 14 pro Max", "12 Pro"))  # ['14', 'Pixel', 'Magic5']
+    print(search_by_model("IPhone X,IPhone 12 Pro,IPhone 14 pro Max", "iphone"))  # ['14', 'Pixel', 'Magic5']
+
+
+
