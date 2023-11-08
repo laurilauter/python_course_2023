c81d9980f8dac0b411f7059f3c00efa3b1119ab2
laurilauter <lauri.lauter@gmail.com>
2023-11-08 23:37:21 +0200

    Getting started.

--- EX/ex09_files/file_handling.py
+++ EX/ex09_files/file_handling.py
@@ -121,7 +121,8 @@ def write_csv_file(filename: str, data: list[list[str]]) -> None:
     :return: None
     """
     with open(filename, "w", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, delimiter=",")
+        # writer = csv.writer(csvfile, delimiter=",")
+        writer = csv.writer(csvfile)
         for row in data:
             writer.writerow(row)
 
