37d97df403523b5793d35bce6bdbd1391cdb5293
laurilauter <lauri.lauter@gmail.com>
2023-11-09 22:08:04 +0200

    Getting started.

---
+++ KT/kt1/exam.py
@@ -0,0 +1,88 @@
+"""KT1."""
+
+
+def capitalize_string(s: str) -> str:
+    """
+    Return capitalized string.
+
+    The first char is capitalized, the rest remain as they are.
+
+    capitalize_string("abc") => "Abc"
+    capitalize_string("ABc") => "ABc"
+    capitalize_string("") => ""
+    """
+    return s.capitalize()
+
+
+def has_seven(nums):
+    """
+    Whether the list has three 7s and no repeated consecutive elements.
+
+    Given a list if ints, return True if the value 7 appears in the list exactly 3 times
+    and no consecutive elements have the same value.
+
+    has_seven([1, 2, 3]) => False
+    has_seven([7, 1, 7, 7]) => False
+    has_seven([7, 1, 7, 1, 7]) => True
+    has_seven([7, 1, 7, 1, 1, 7]) => False
+    """
+    pass
+
+
+def parse_call_log(call_log: str) -> dict:
+    """
+    Parse calling logs to find out who has been calling to whom.
+
+    There is a process, where one person calls to another,
+    then this another person call yet to another person etc.
+    The log consists of several those call-chains, separated by comma (,).
+    One call-chain consists of 2 or more names, separated by colon (:).
+
+    The function should return a dict where the key is a name
+    and the value is all the names the key has called to.
+
+    Each name has to be in the list only once.
+    The order of the list or the keys in the dictionary are not important.
+
+    Input:
+    - consists of 0 or more "chains"
+    - chains are separated by comma (,)
+    - one chain consists of 2 or more names
+    - name is 1 or more symbols long
+    - there are no commas nor colons in the name
+    - names are separated by colon (:)
+
+    parse_call_log("") => {}
+    parse_call_log("ago:kati,mati:malle") => {"ago": ["kati"], "mati": ["malle"]}
+    parse_call_log("ago:kati,ago:mati,ago:kati") => {"ago": ["kati", "mati"]}
+    parse_call_log("ago:kati:mati") => {"ago": ["kati"], "kati": ["mati"]}
+    parse_call_log("mati:kalle,kalle:malle:mari:juri,mari:mati") =>
+    {'mati': ['kalle'], 'kalle': ['malle'], 'malle': ['mari'], 'mari': ['juri', 'mati']}
+
+    :param call_log: the whole log as string
+    :return: dictionary with call information
+    """
+    pass
+
+
+def mirror_ends(s: str) -> str:
+    """
+    Return the first non-matching symbol pair from both ends.
+
+    The function has to be recursive. No loops allowed!
+
+    Starting from the beginning and end, find the first symbol pair which does not match.
+    If the input string is a palindrome (the same in reverse) then return "" (empty string).
+
+    mirror_ends("abc") => "ac"
+    mirror_ends("aba") => ""
+    mirror_ends("abca") => "bc"
+    mirror_ends("abAAca") => "bc"
+    mirror_ends("") => ""
+    """
+    pass
+
+
+if __name__ == '__main__':
+
+    print(capitalize_string("abc"))
