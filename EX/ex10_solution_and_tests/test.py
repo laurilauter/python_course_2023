58d7d7fc56fe1b9750a0a4edcf624c211b5aca3b
laurilauter <lauri.lauter@gmail.com>
2023-11-10 22:36:09 +0200

    Getting started.

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -10,4 +10,18 @@ def test_students_study_during_day():
     This case represents the time period of a day and coffee is present.
     Expected result: True.
     """
+    assert students_study(18, True) is True
+    assert students_study(24, True) is True
+    assert students_study(18, False) is True
+    assert students_study(24, False) is True
+
+    assert students_study(1, True) is False
+    assert students_study(4, True) is False
+    assert students_study(1, False) is False
+    assert students_study(4, False) is False
+
+    assert students_study(5, True) is True
+    assert students_study(17, True) is True
+    assert students_study(5, False) is False
+    assert students_study(17, False) is False
     assert students_study(12, True) is True
