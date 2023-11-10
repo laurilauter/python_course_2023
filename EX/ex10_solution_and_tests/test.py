1338f08a68ae5360214bed47d0bc064d8b55c134
laurilauter <lauri.lauter@gmail.com>
2023-11-10 23:29:48 +0200

    Getting started.

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -46,7 +46,7 @@ def test_students_study_random():
 
 
 #  LOTTERY TESTS
-def test_lottery():
+def test_lottery_all_cases():
     """The lottery."""
     assert lottery(5, 5, 5) == 10
     assert lottery(-5, -5, -5) == 5
@@ -54,9 +54,34 @@ def test_lottery():
     assert lottery(4, 4, 4) == 5
     assert lottery(2, 2, 1) == 0
     assert lottery(2, 3, 1) == 1
-
     assert lottery(2, 3, 2) == 0
     assert lottery(2, 3, 3) == 1
+
+
+def test_fruit_order_all_zero():
+    """Fruit order."""
+    assert fruit_order(0, 0, 0) == 0
+
+
+def test_fruit_order_zero_amount_zero_small():
+    """Fruit order."""
+    assert fruit_order(0, 6, 0) == -1
+
+
+def test_fruit_order_zero_amount_zero_big():
+    """Fruit order."""
+    assert fruit_order(6, 0, 0) == 0
+
+
+def test_fruit_order_zero_amount_others_not_zero():
+    """Fruit order."""
+    assert fruit_order(6, 6, 0) == -1
+
+
+def test_fruit_order_zero_only_big_exact_match():
+    """Fruit order."""
+    assert fruit_order(0, 2, 10) == 0
+
 
 
 if __name__ == '__main__':
