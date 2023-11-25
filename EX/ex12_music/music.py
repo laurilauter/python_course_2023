e9712def0bee2e920b7c2859543fecc4d8b62c5f
laurilauter <lauri.lauter@gmail.com>
2023-11-25 22:35:35 +0200

    Getting started.

--- EX/ex12_music/music.py
+++ EX/ex12_music/music.py
@@ -200,8 +200,8 @@ class Chords:
 
         Add whatever you need to make this class function.
         """
-        # self.chords = []
-        self.chords = {}
+        self.chords = []
+        # self.chords = {}
 
     def add(self, chord: Chord) -> None:
         """
@@ -211,12 +211,15 @@ class Chords:
 
         :param chord: Chord to be added.
         """
-        if chord not in self.chords.values():
-            #self.chords.append(chord)
-            self.chords[chord.chord_name] = chord
+        if chord not in self.chords:
+            self.chords.append(chord)
         else:
             raise ChordOverlapException("Chord already in Chords.")
-        print(self.chords)
+
+        # if chord not in self.chords.values():
+        #     self.chords[chord.chord_name] = chord
+        # else:
+        #     raise ChordOverlapException("Chord already in Chords.")
 
     def get(self, first_note: Note, second_note: Note, third_note: Note = None) -> Chord | None:
         """
@@ -242,12 +245,18 @@ class Chords:
         """
         requested_chord = Chord(first_note, second_note, 'Requested', third_note)
 
-        for c in self.chords.values():
-            print(c)
-            #if c == requested_chord:
+        for c in self.chords:
+            # if c == requested_chord:
             if set(c.chord_notes) == set(requested_chord.chord_notes):
                 return c
         return None
+
+        # for c in self.chords.values():
+        #     print(c)
+        #     #if c == requested_chord:
+        #     if set(c.chord_notes) == set(requested_chord.chord_notes):
+        #         return c
+        # return None
 
 
 class DuplicateNoteNamesException(Exception):
