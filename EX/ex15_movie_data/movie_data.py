1102eb1cd894731060c6fe0a4bf9004b3cb88389
laurilauter <lauri.lauter@gmail.com>
2023-12-23 23:23:58 +0200

    Ongoing development

--- EX/ex15_movie_data/movie_data.py
+++ EX/ex15_movie_data/movie_data.py
@@ -37,8 +37,10 @@ class MovieData:
         """
         try:
             self.movies = pd.DataFrame(pd.read_csv(movies_filename))
-            self.ratings = pd.DataFrame(pd.read_csv(ratings_filename)).drop(columns=['userId', 'timestamp'], axis=1)
-            self.tags = pd.DataFrame(pd.read_csv(tags_filename)).drop(columns=['userId', 'timestamp'], axis=1)
+            # self.ratings = pd.DataFrame(pd.read_csv(ratings_filename)).drop(columns=['userId', 'timestamp'], axis=1)
+            self.ratings = pd.DataFrame(pd.read_csv(ratings_filename))
+            # self.tags = pd.DataFrame(pd.read_csv(tags_filename)).drop(columns=['userId', 'timestamp'], axis=1)
+            self.tags = pd.DataFrame(pd.read_csv(tags_filename))
         except ValueError:
             raise ValueError("Could not load all data.")
 
