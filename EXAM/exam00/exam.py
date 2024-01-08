50a50d9cbc29a34524793648c479cef378d2e3f0
laurilauter <lauri.lauter@gmail.com>
2024-01-08 16:56:24 +0200

    Ongoing development

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -212,7 +212,9 @@ def create_student(name: str, grades: list, credit_points: int) -> Student:
     Round the average grade up to three decimal places.
     If the list of grades is empty, the average grade will be 0.
     """
-    avg_grade = round(sum(grades) / len(grades), 3)
+    avg_grade = 0
+    if len(grades) > 0:
+        avg_grade = round(sum(grades) / len(grades), 3)
     return Student(name, avg_grade, credit_points)
 
 
