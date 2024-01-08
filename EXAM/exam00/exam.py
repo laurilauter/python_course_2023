1f5654af0cd45875bf9e3f68afdfd8679fe1958e
laurilauter <lauri.lauter@gmail.com>
2024-01-08 19:53:04 +0200

    Ongoing development

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -279,7 +279,7 @@ def get_ordered_students(students: list) -> list:
 
     credit points (higher first), average_grade (higher first), name (a to z).
     """
-    pass
+    return sorted(students, key=lambda student: (student.credit_points, student.average_grade, student.name))
 
 
 class Room:
