92c36f667703c8825b26b4ba418f15011c4e664d
laurilauter <lauri.lauter@gmail.com>
2024-01-11 23:31:23 +0200

    Practicing

--- EXAM/exam03/exam.py
+++ EXAM/exam03/exam.py
@@ -271,7 +271,7 @@ class Cinema:
         """
         for movie in self.movies:
             if movie.title.lower() == title.lower():
-                return movie.genre
+                return movie.genre.lower()
         return "No such movie"
 
     def get_movie_timetable(self) -> list[Movie]:
